4293447fb7fa3cdf8d23c917feed352fdcf2c358
sunmi An <aseonmi220@gmail.com>
2025-05-21 23:00:16 +0900

    프로젝트 생성

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        {/* 헤더가 필요한 컴포넌트  */}
+        <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/mypage" element={<MyPage />} />
+        </Route>
+
+        {/* 헤더가 필요없는 컴포넌트  */}
+        <Route path="/login" element={<Login /} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
